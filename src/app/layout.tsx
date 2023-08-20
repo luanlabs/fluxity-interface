4cf9e33a3ca9d99a7c97882b6eed99be2eb23361
ruveyda-kisla <ruveydakisla572@gmail.com>
2023-08-20 14:42:34 +0330

    fix: reorganized imports

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
-import '../styles/registry';
 import type { Metadata } from 'next';
 import { Inter } from 'next/font/google';
+
+import '../styles/globals.css';
 import StyledComponentsRegistry from '../styles/registry';
 
 const inter = Inter({ subsets: ['latin'] });
@@ -19,9 +20,19 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <StyledComponentsRegistry>
-          <header>head</header>
-          <aside>aside</aside>
-          <section> {children}</section>
+          <main className="bg-alabaster h-screen px-8 pt-[9px] pb-7">
+            <header className="bg-white h-[72px] rounded-[14px] mb-3">
+              head
+            </header>
+            <section className="inline-flex basis-full gap-4 w-full h-5/6">
+              <aside className="bg-white basis-3/12 md:basis-2/12 rounded-[14px]">
+                aside
+              </aside>
+              <article className="bg-white basis-9/12 md:basis-10/12 rounded-[14px]">
+                {children}
+              </article>
+            </section>
+          </main>
         </StyledComponentsRegistry>
       </body>
     </html>
