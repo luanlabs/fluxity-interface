2b675e61ab3d8ffc2da59c56660364b46ccab8b0
Dogukan Celik <dousdogukan@gmail.com>
2023-09-04 22:31:24 +0330

    chore: implement CreateStream

---
+++ src/app/create-stream/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+import React from 'react';
+import CreateStreamMainCard from 'src/containers/CreateStreamMainCard';
+
+const CreateStream = () => {
+  return <CreateStreamMainCard />;
+};
+
+export default CreateStream;
