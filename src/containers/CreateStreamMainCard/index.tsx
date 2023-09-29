2963aa06102402dba979e3a1f12626e0107fdaa4
Dogukan Celik <dousdogukan@gmail.com>
2023-09-29 19:22:19 +0330

    fix: Pr #8

--- src/containers/CreateStreamMainCard/index.tsx
+++ src/containers/CreateStreamMainCard/index.tsx
@@ -22,7 +22,7 @@ type FormValues = {
 const CreateStream = () => {
   const form = useForm<FormValues>({
     defaultValues: {
-      rateTime: 'Monthly',
+      rateTime: 'monthly',
     },
   });
   const { handleSubmit, control, getValues, setValue, watch } = form;
