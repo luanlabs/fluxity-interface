1d91fa2652f182c0edc51d3341c989ed795bf582
Ruveyda Kışla <ruveydakisla572@gmail.com>
2023-10-15 00:40:40 +0330

    chore: add home components

--- src/containers/CreateStreamCardPurpleCard/index.tsx
+++ src/containers/CreateStreamCardPurpleCard/index.tsx
@@ -6,17 +6,20 @@ import fluxityLogo from '../../../public/images/fluxity.svg';
 
 const CreateStreamCardPurpleCard = () => {
   return (
-    <div className="w-[480px]">
+    <div className="w-1/2">
       <CCard
         className="flex flex-col px-[21px] py-[13px] w-full h-full"
         bgColor="#f5ebff"
         borderColor="#BE7CFF"
       >
-        <h1 className="text-[22px] mb-[25px] w-[32%] text-richlavender">Create Stream</h1>
-        <p className="text-[16px] w-[60%] mb-[30px] text-midnightblue leading-[20px]">
-          Initiate token streams effortlessly; streams tailored to respond to your needs.
+        <h1 className="text-[22px] mb-[25px] w-[32%] whitespace-nowrap text-richlavender">
+          Create Stream
+        </h1>
+        <p className="text-[16px] w-[70%] mb-[30px] text-midnightblue leading-[20px]">
+          Initiate token streams effortlessly; streams tailored to respond to
+          your needs.
         </p>
-        <div className=" flex items-center gap-4 pb-2">
+        <div className="flex items-center gap-4 mt-[20px] pb-2">
           <CButton
             kind="simple"
             color="purple"
--- src/containers/DocumentationCard/index.tsx
+++ src/containers/DocumentationCard/index.tsx
@@ -5,19 +5,26 @@ import CButton from '../../components/CButton';
 
 const DocumentationCard = () => {
   return (
-    <div className="w-[480px]">
+    <div className="w-1/2">
       <CCard
         className="flex flex-col px-[21px] py-[13px] w-full h-full"
         bgColor="mistyrose"
         borderColor="#D98F7F"
       >
-        <h1 className="text-[22px] mb-[10px] w-[32%] text-darkcoral">Documentation</h1>
+        <h1 className="text-[22px] mb-[10px] w-[32%] text-darkcoral">
+          Documentation
+        </h1>
         <p className="text-[16px] w-[70%] mb-[30px] text-midnightblue leading-[20px]">
-          Explore the opportunities created by token streaming and how Fluxity provides
-          users with the most unique experience.{' '}
+          Explore the opportunities created by token streaming and how Fluxity
+          provides users with the most unique experience.{' '}
         </p>
         <div className="flex items-center gap-4 mt-4 pb-2">
-          <CButton kind="simple" color="orange" content="Read Whitepaper" logo="" />
+          <CButton
+            kind="simple"
+            color="orange"
+            content="Read Whitepaper"
+            logo=""
+          />
           <a href="#" className="text-[14px]">
             Learn more
           </a>
