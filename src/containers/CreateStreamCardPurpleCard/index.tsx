3f2bb79d66a23082a9b040a64ff2d47a8affe8aa
Ruveyda Kışla <ruveydakisla572@gmail.com>
2023-10-19 02:14:38 +0330

    chore: fix Home cards

--- src/containers/CreateStreamCardPurpleCard/index.tsx
+++ src/containers/CreateStreamCardPurpleCard/index.tsx
@@ -19,18 +19,18 @@ const CreateStreamCardPurpleCard = () => {
   return (
     <div className="w-full">
       <CCard
-        className="flex flex-col px-[21px] py-[13px] w-full h-full"
+        className="flex flex-col relative px-[21px] py-[13px] w-full h-[230px]"
         bgColor="#f5ebff"
         borderColor="#BE7CFF"
       >
-        <h1 className="text-[22px] mb-[25px] w-[32%] whitespace-nowrap text-richlavender">
+        <h1 className="text-[22px] mb-[25px] whitespace-nowrap text-richlavender">
           Create Stream
         </h1>
-        <p className="text-[16px] w-[70%] mb-[30px] text-midnightblue leading-[20px]">
+        <p className="text-[16px] w-[70%] text-midnightblue leading-[20px]">
           Initiate token streams effortlessly; streams tailored to respond to
           your needs.
         </p>
-        <div className="flex items-center gap-4 mt-[20px] pb-2">
+        <div className="absolute left-4 bottom-4 flex items-center gap-4">
           <CButton
             onClick={handleClick}
             kind="simple"
