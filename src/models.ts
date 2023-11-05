9204ee784d297ab80bb0f587f7aaa64377dbaeaa
Dogukan Celik <dousdogukan@gmail.com>
2023-11-05 20:04:03 +0330

    chore: add rate types

--- src/models.ts
+++ src/models.ts
@@ -1,29 +1,35 @@
-import {
-  GroupBase,
-  StylesConfig,
-  MultiValue,
-  SingleValue,
-  ActionMeta,
-} from 'react-select';
+import { GroupBase, StylesConfig, MultiValue, SingleValue, ActionMeta } from 'react-select';
 
-export interface SelectItemType {
+import { rates } from './constants/rates';
+
+export interface ISelectItem<T> {
   label: string;
-  value: string;
+  value: T;
 }
 
-export interface SelectTokenType extends SelectItemType {
+export type ISelectItemString = ISelectItem<string>;
+
+export type Rates = keyof typeof rates;
+export type RateValue = ISelectItem<keyof typeof rates>;
+
+export interface SelectTokenType extends ISelectItemString {
   icon: string;
 }
 
 export type ReactSelectType = StylesConfig<
-  SelectItemType,
+  ISelectItemString,
   boolean,
-  GroupBase<SelectItemType>
+  GroupBase<ISelectItemString>
 >;
 
 export type ReactSelectOnChangeType = (
-  newValue: MultiValue<SelectItemType> | SingleValue<SelectItemType>,
-  actionMeta: ActionMeta<SelectItemType>,
+  newValue: MultiValue<ISelectItemString> | SingleValue<ISelectItemString>,
+  actionMeta: ActionMeta<ISelectItemString>,
+) => void;
+
+export type RateSelectOnChangeType = (
+  newValue: MultiValue<Rates> | SingleValue<Rates>,
+  actionMeta: ActionMeta<Rates>,
 ) => void;
 
 export type SvgProps = {
