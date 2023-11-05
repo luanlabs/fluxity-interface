ecca986ae2204fc3b111550e95c65c6c5360d622
Dogukan Celik <dousdogukan@gmail.com>
2023-11-05 19:22:56 +0330

    feat: implement createStream function and call create_stream contract

---
+++ src/futures/CreateStream/createStream.ts
@@ -0,0 +1,23 @@
+import { FormValues } from 'src/containers/CreateStreamMainCard';
+import { FLUXITY_CONTRACT } from 'src/constants/contracts';
+import ToScVal from 'src/utils/createStream/scVal';
+import getServer from 'src/utils/createStream/getSever';
+import getAccount from 'src/utils/createStream/getAccount';
+import getContract from 'src/utils/createStream/getContract';
+import createTransaction from './baseTransaction';
+
+const createStream = async (params: FormValues, address: string) => {
+  const account = await getAccount(address);
+
+  const server = getServer();
+  const contract = getContract(FLUXITY_CONTRACT);
+
+  const paramsScVal = ToScVal.toXdr(params, address);
+
+  const call = contract.call('create_stream', paramsScVal);
+  const xdr = createTransaction(account, call);
+
+  return await server.prepareTransaction(xdr);
+};
+
+export default createStream;
