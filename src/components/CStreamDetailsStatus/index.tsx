c19113649df70fec7c09ab6e6068c30e7ba9cad6
Dogukan Celik <dousdogukan@gmail.com>
2023-11-19 23:15:04 +0330

    fix: status types

--- src/components/CStreamDetailsStatus/index.tsx
+++ src/components/CStreamDetailsStatus/index.tsx
@@ -1,19 +1,26 @@
 import cn from 'classnames';
+
+import capitalizeFirstLetter from 'src/utils/capitalizeFirstLetter';
+
+export type status = 'ongoing' | 'expired' | 'pending';
 interface CStreamDetailsStatusProps {
-  type: 'Active' | 'Expired' | 'Pending';
+  type: status;
+  label: status;
 }
 
-const CStreamDetailsStatus = ({ type }: CStreamDetailsStatusProps) => {
-  let statusStyle;
-  if (type === 'Active') {
+const CStreamDetailsStatus = ({ type, label }: CStreamDetailsStatusProps) => {
+  let statusStyle = 'bg-brightYellow border-burntOrange text-burntOrange';
+  if (type === 'ongoing') {
     statusStyle = 'border-forestGreen bg-paleMint text-forestGreen';
-  } else if (type === 'Expired') {
+  } else if (type === 'expired') {
     statusStyle = 'bg-lightGrayishBlue border-darkGrayishTeal text-darkGrayishTeal';
-  } else if (type === 'Pending') {
-    statusStyle = 'bg-brightYellow border-burntOrange text-burntOrange';
   }
 
-  return <div className={cn('border rounded-[63px] px-4 py-2', statusStyle)}>{type}</div>;
+  return (
+    <div className={cn('border rounded-[63px] px-4 py-2', statusStyle)}>
+      {capitalizeFirstLetter(label)}
+    </div>
+  );
 };
 
 export default CStreamDetailsStatus;
