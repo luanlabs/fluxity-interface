ed7d7d56b49bb4c9d5e6b38ca9cdabf2d737f4e2
Dogukan Celik <dousdogukan@gmail.com>
2023-11-28 00:14:01 +0330

    chore: completing the StreamProgress component

--- src/containers/StreamDetails/StreamProgress.tsx
+++ src/containers/StreamDetails/StreamProgress.tsx
@@ -1,18 +1,20 @@
-import React, { useEffect, useState } from 'react';
-import { ProgressStyle } from './style';
+import React from 'react';
 
 interface ProgressProps {
   progressValue: number;
 }
 
-const App = ({ progressValue }: ProgressProps) => {
+const StreamProgress = ({ progressValue }: ProgressProps) => {
   return (
-    <ProgressStyle>
-      <div className="bar">
-        <div className="progress" style={{ width: `${progressValue}%` }}></div>
-      </div>
-    </ProgressStyle>
+    <div className=" bg-darkOrchid w-full h-[48px] rounded-[9px] overflow-hidden relative border border-white">
+      <div
+        className="w-full bg-darkOrchid h-[48px] z-10 absolute top-0 left-0 transition-all ease-linear duration-300"
+        style={{ transform: `translateX(${progressValue}%)` }}
+      ></div>
+
+      <div className="w-full h-[48px] bg-gradient-to-r from-slate-50 via-green-50 to-softGreen absolute top-0 left-0 "></div>
+    </div>
   );
 };
 
-export default App;
+export default StreamProgress;
