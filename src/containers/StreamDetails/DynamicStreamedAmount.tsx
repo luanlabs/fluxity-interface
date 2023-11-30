14bdbfaefc53ea9749ef0ceaec3e84f3f20d8a07
Dogukan Celik <dousdogukan@gmail.com>
2023-12-01 00:27:39 +0330

    fix: change amount to DynamicStreamedAmount

---
+++ src/containers/StreamDetails/DynamicStreamedAmount.tsx
@@ -0,0 +1,24 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+
+interface DynamicStreamedAmountProps {
+  token: string;
+  streamAmount: string;
+}
+
+const DynamicStreamedAmount = ({ token, streamAmount }: DynamicStreamedAmountProps) => {
+  return (
+    <div className="flex flex-col items-center justify-center w-full">
+      <div className="text-[40px] flex justify-center w-[100%] gap-2">
+        <div className="text-center">
+          <h2 className="text-[40px]">+{streamAmount}</h2>
+        </div>
+        <div className="w-[31%]">
+          <p>{token}</p>
+        </div>
+      </div>
+      <p className="text-base">Total amount streamed</p>
+    </div>
+  );
+};
+
+export default DynamicStreamedAmount;
