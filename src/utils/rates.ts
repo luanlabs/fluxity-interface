07678c8fad7ef43d055e6833c71dd3d4b8ed709d
Dogukan Celik <dousdogukan@gmail.com>
2023-12-03 00:14:15 +0330

    fix: fixed numberToRate for PR #32

--- src/utils/rates.ts
+++ src/utils/rates.ts
@@ -2,10 +2,18 @@ import { rates } from 'src/constants/rates';
 
 export const rateToNumber = (rate: keyof typeof rates): number => rates[rate];
 
-export const numberToRate = (number: number): keyof typeof rates | undefined => {
-  const entry = Object.entries(rates).find(([, value]) => value === number);
-
-  return entry ? (entry[0] as keyof typeof rates) : undefined;
+export const numberToRate = (number: number): string | undefined => {
+  if (number === rates.daily) {
+    return 'Day';
+  } else if (number === rates.weekly) {
+    return 'Week';
+  } else if (number === rates.monthly) {
+    return 'Month';
+  } else if (number === rates.quarterly) {
+    return 'Quarter';
+  } else if (number === rates.annually) {
+    return 'Annual';
+  }
 };
 
 export default rateToNumber;
