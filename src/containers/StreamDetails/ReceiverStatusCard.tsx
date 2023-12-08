8e1551b0fcc7dbf73044a9a663f97c390dae00ae
Dogukan Celik <dousdogukan@gmail.com>
2023-12-08 13:00:10 +0330

    chore: use isWithdraw func

--- src/containers/StreamDetails/ReceiverStatusCard.tsx
+++ src/containers/StreamDetails/ReceiverStatusCard.tsx
@@ -2,7 +2,8 @@ import BN from 'src/utils/BN';
 import CButton from 'src/components/CButton';
 import CPageCard from 'src/components/CPageCard';
 import CSummaryField from 'src/components/CSummaryField';
-import { calculateStreamAmounts } from 'src/utils/calculateStreamAmount';
+import calculateStreamAmounts from 'src/utils/calculateStreamAmount';
+import isWithdraw from 'src/utils/isWithdraw';
 
 import withdrawLogo from '/public/images/withdrawSolid.svg';
 
@@ -12,6 +13,7 @@ interface ReceiverStatusCardProps {
   startDate: number;
   endDate: number;
   cliffDate: number;
+  isCanellable: boolean;
 }
 
 const ReceiverStatusCard = ({
@@ -20,6 +22,7 @@ const ReceiverStatusCard = ({
   startDate,
   endDate,
   cliffDate,
+  isCanellable,
 }: ReceiverStatusCardProps) => {
   const available = calculateStreamAmounts(
     startDate,
@@ -28,6 +31,14 @@ const ReceiverStatusCard = ({
     amount,
   ).receiverAmount.minus(withdrawn);
 
+  const withdraw = isWithdraw(
+    startDate,
+    cliffDate,
+    Number(amount),
+    Number(withdrawn),
+    isCanellable,
+  );
+
   const ReceiverStatusCardTitle = (
     <div className="w-full flex justify-between items-center pb-4 pl-4">
       <h1 className="text-2xl text-midnightBlue">Status</h1>
@@ -35,14 +46,17 @@ const ReceiverStatusCard = ({
         variant="simple"
         color="outline"
         content="Withdraw"
+        disabled={withdraw}
         logo={withdrawLogo}
-        className="!px-3 !py-2 h-[40px]"
+        className={`!px-3 !py-2 h-[40px] ${
+          withdraw && '!text-softGray !border-softGray hover:!bg-transparent'
+        }`}
       />
     </div>
   );
 
   return (
-    <div className="w-[580px]">
+    <div className="w-full">
       <CPageCard title={ReceiverStatusCardTitle} className="px-3 py-4 mb-4 w-full">
         <div className="grid gap-2 text-midnightBlue">
           <CSummaryField label="Available" value={available.toFixed(3)} />
