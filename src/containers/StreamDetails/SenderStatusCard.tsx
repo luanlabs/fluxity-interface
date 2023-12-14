64e6f4e62c6373d78aa6896b0b438f33ee558f1e
Dogukan Celik <dousdogukan@gmail.com>
2023-12-14 21:07:05 +0330

    chore : Building the cancel operation

--- src/containers/StreamDetails/SenderStatusCard.tsx
+++ src/containers/StreamDetails/SenderStatusCard.tsx
@@ -1,14 +1,29 @@
+import { useState } from 'react';
+
+import BN from 'src/utils/BN';
+import timeout from 'src/utils/timeout';
+import toast from 'src/components/CToast';
 import CButton from 'src/components/CButton';
 import CPageCard from 'src/components/CPageCard';
+import { useAppSelector } from 'src/hooks/useRedux';
+import CProcessModal from 'src/components/CProcessModal';
 import CSummaryField from 'src/components/CSummaryField';
+import CModalSuccess from 'src/components/CModalSuccess';
+import cancelStream from 'src/features/soroban/cancelStream';
+import signTransaction from 'src/utils/soroban/signTransaction';
+import sendTransaction from 'src/features/soroban/sendTransaction';
 import calculateStreamAmounts from 'src/utils/calculateStreamAmount';
+import finalizeTransaction from 'src/utils/soroban/finalizeTransaction';
 
 interface SenderStatusCardProps {
   amount: string;
   startDate: number;
   endDate: number;
   cliffDate: number;
+  receiver: string;
+  token: string;
   isCancellable: boolean;
+  id: string;
 }
 
 const SenderStatusCard = ({
@@ -16,8 +31,76 @@ const SenderStatusCard = ({
   startDate,
   endDate,
   cliffDate,
+  receiver,
+  token,
   isCancellable,
+  id,
 }: SenderStatusCardProps) => {
+  const address = useAppSelector((state) => state.user.address);
+
+  const [isApprovalOpen, setIsApprovalOpen] = useState(false);
+  const [isCancelStreamConfirmOpen, setIsCancelStreamConfirmOpen] = useState(false);
+  const [isReclamationModalOpen, setIsReclamationModalOpen] = useState(false);
+
+  const handleCancelClick = async () => {
+    setIsApprovalOpen(true);
+
+    const cancelStreamXdr = await cancelStream(id, address);
+
+    let signedXdr;
+    let tx;
+
+    try {
+      signedXdr = await signTransaction(address, cancelStreamXdr);
+      tx = await sendTransaction(signedXdr);
+    } catch (e) {
+      setIsApprovalOpen(false);
+      toast('error', 'Failed to sign the transaction');
+
+      return;
+    }
+
+    setIsApprovalOpen(false);
+    await timeout(100);
+    setIsReclamationModalOpen(true);
+
+    if (tx) {
+      const finalize = await finalizeTransaction(tx.hash);
+
+      if (!finalize) {
+        setIsReclamationModalOpen(false);
+        toast('error', 'Token stream cancellation unsuccessful');
+
+        return;
+      }
+    } else {
+      setIsReclamationModalOpen(false);
+      return;
+    }
+
+    setIsReclamationModalOpen(false);
+    await timeout(100);
+    setIsCancelStreamConfirmOpen(true);
+  };
+
+  const handleModalButton = () => {
+    setIsCancelStreamConfirmOpen(false);
+  };
+
+  const senderAmount = calculateStreamAmounts(
+    startDate,
+    endDate,
+    cliffDate,
+    amount,
+  ).senderAmount.toFixed(3);
+
+  const receiveAmount = calculateStreamAmounts(
+    startDate,
+    endDate,
+    cliffDate,
+    amount,
+  ).receiverAmount.toFixed(3);
+
   const SenderStatusCardTitle = (
     <div className="w-full flex justify-between items-center pb-4 pl-4">
       <h1 className="text-2xl text-midnightBlue">Status</h1>
@@ -29,6 +112,7 @@ const SenderStatusCard = ({
         className={`w-[146px] !py-2 h-[40px] text-[14px] ${
           !isCancellable && '!text-softGray !border-softGray hover:!bg-transparent'
         }`}
+        onClick={handleCancelClick}
       />
     </div>
   );
@@ -37,16 +121,35 @@ const SenderStatusCard = ({
     <div className="w-full">
       <CPageCard title={SenderStatusCardTitle} className="px-3 py-4 mb-4 w-full">
         <div className="grid gap-2 text-midnightBlue">
-          <CSummaryField
-            label="Remaining amount"
-            value={calculateStreamAmounts(
-              startDate,
-              endDate,
-              cliffDate,
-              amount,
-            ).senderAmount.toFixed(3)}
-          />
+          <CSummaryField label="Remaining amount" value={senderAmount} fieldSize="large" />
         </div>
+
+        <CProcessModal
+          isOpen={isApprovalOpen}
+          setIsOpen={setIsApprovalOpen}
+          title="Waiting for stream cancellation approval"
+        />
+
+        <CProcessModal
+          isOpen={isReclamationModalOpen}
+          setIsOpen={setIsReclamationModalOpen}
+          title="Waiting for amount reclamation completion"
+        />
+
+        <CModalSuccess
+          successLogoColor="green"
+          title="Token withdrawal successful"
+          streamId={id}
+          to={receiver}
+          token={token}
+          amountTitle="Amount"
+          amount={new BN(amount).toFixed(3)}
+          buttonVariant="simple"
+          buttonText="Close"
+          isOpen={isCancelStreamConfirmOpen}
+          setIsOpen={setIsCancelStreamConfirmOpen}
+          onClick={handleModalButton}
+        />
       </CPageCard>
     </div>
   );
--- src/features/soroban/cancelStream.ts
+++ src/features/soroban/cancelStream.ts
@@ -6,13 +6,18 @@ import getContract from 'src/utils/createStream/getContract';
 import createTransaction from 'src/utils/soroban/baseTransaction';
 
 const cancelStream = async (id: string, address: string) => {
+  console.log(address);
+
   const account = await getAccount(address);
+  console.log('sfffffs');
 
   const server = getServer();
   const contract = getContract(FLUXITY_CONTRACT);
+  console.log(ToScVal.u64(id));
 
   const call = contract.call('cancel_stream', ToScVal.u64(id));
   const xdr = createTransaction(account, call);
+  console.log('ss');
 
   return await server.prepareTransaction(xdr);
 };
