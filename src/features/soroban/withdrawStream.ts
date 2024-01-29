b926ed30e0e47b54dc17a49e19b79a013c7f0310
dousdogukan <dousdogukan@gmail.com>
2024-01-30 00:27:49 +0330

    fix: fix withdraw function arguments

--- src/features/soroban/withdrawStream.ts
+++ src/features/soroban/withdrawStream.ts
@@ -1,17 +1,18 @@
+import { Contract } from 'stellar-sdk';
+
 import { FLUXITY_CONTRACT } from 'src/constants/contracts';
 import ToScVal from 'src/utils/createStream/scVal';
 import getServer from 'src/utils/createStream/getServer';
 import getAccount from 'src/utils/createStream/getAccount';
-import getContract from 'src/utils/createStream/getContract';
 import createTransaction from 'src/utils/soroban/baseTransaction';
 
-const withdrawStream = async (id: string, amount: bigint, address: string) => {
+const withdrawStream = async (id: string, address: string) => {
   const account = await getAccount(address);
 
   const server = getServer();
-  const contract = getContract(FLUXITY_CONTRACT);
+  const contract = new Contract(FLUXITY_CONTRACT);
 
-  const call = contract.call('withdraw_stream', ToScVal.u64(id), ToScVal.i128(amount));
+  const call = contract.call('withdraw_stream', ToScVal.u64(id), ToScVal.i128(BigInt(0)));
   const xdr = createTransaction(account, call);
 
   return await server.prepareTransaction(xdr);
