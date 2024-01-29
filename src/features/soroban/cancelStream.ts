34185e88c7aa1c890b7de4c635e3b069b60ba2f2
dousdogukan <dousdogukan@gmail.com>
2024-01-30 00:35:38 +0330

    chore: cancel stream function

--- src/features/soroban/cancelStream.ts
+++ src/features/soroban/cancelStream.ts
@@ -1,23 +1,20 @@
+import { Contract } from 'stellar-sdk';
+
 import { FLUXITY_CONTRACT } from 'src/constants/contracts';
 import ToScVal from 'src/utils/createStream/scVal';
 import getServer from 'src/utils/createStream/getServer';
 import getAccount from 'src/utils/createStream/getAccount';
-import getContract from 'src/utils/createStream/getContract';
 import createTransaction from 'src/utils/soroban/baseTransaction';
 
 const cancelStream = async (id: string, address: string) => {
-  console.log(address);
-
   const account = await getAccount(address);
-  console.log('sfffffs');
 
   const server = getServer();
-  const contract = getContract(FLUXITY_CONTRACT);
+  const contract = new Contract(FLUXITY_CONTRACT);
   console.log(ToScVal.u64(id));
 
   const call = contract.call('cancel_stream', ToScVal.u64(id));
   const xdr = createTransaction(account, call);
-  console.log('ss');
 
   return await server.prepareTransaction(xdr);
 };
