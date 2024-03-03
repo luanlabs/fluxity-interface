8341fa1dd1982f5105d71161644aa8ffe5f6447f
dousdogukan <dousdogukan@gmail.com>
2024-03-03 20:08:44 +0330

    fix: reset cancellableStream

--- src/containers/CancellableStream/index.tsx
+++ src/containers/CancellableStream/index.tsx
@@ -1,5 +1,6 @@
 import CToggle from 'src/components/CToggle';
 import CLabel from 'src/components/CLabel';
+import { useEffect, useState } from 'react';
 
 export type ToggleStatus = 'ON' | 'OFF';
 
@@ -7,12 +8,28 @@ interface CancellableStreamProps {
   onChange: (value: ToggleStatus) => void;
   tooltipDetails: string;
   tooltipTitle: string;
+  value: any;
 }
 
-const CancellableStream = ({ onChange, tooltipTitle, tooltipDetails }: CancellableStreamProps) => {
+const CancellableStream = ({
+  onChange,
+  tooltipTitle,
+  tooltipDetails,
+  value,
+}: CancellableStreamProps) => {
+  const [isEnabled, setIsEnabled] = useState(false);
+
   const handleToggleChecker = (value: boolean) => {
     onChange(value ? 'ON' : 'OFF');
   };
+
+  useEffect(() => {
+    if (value === 'OFF') {
+      setIsEnabled(false);
+    } else {
+      setIsEnabled(true);
+    }
+  }, [value, isEnabled]);
 
   return (
     <div className="w-full flex items-center justify-between sm:mb-10">
@@ -21,7 +38,7 @@ const CancellableStream = ({ onChange, tooltipTitle, tooltipDetails }: Cancellab
         <CLabel tooltipDetails={tooltipDetails} tooltipTitle={tooltipTitle} />
       </div>
       <div className="flex items-center">
-        <CToggle onChange={handleToggleChecker} />
+        <CToggle onChange={handleToggleChecker} isEnabled={isEnabled} setIsEnabled={setIsEnabled} />
       </div>
     </div>
   );
