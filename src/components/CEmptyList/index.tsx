bb1515d53eb45ef8e506b991a46ded6b8abdc693
dousdogukan <dousdogukan@gmail.com>
2024-03-05 18:01:41 +0330

    fix: fixed logo name

--- src/components/CEmptyList/index.tsx
+++ src/components/CEmptyList/index.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image';
 
-import noStreams from 'public/images/noStreams.svg';
+import emptyLogo from 'public/images/noStreams.svg';
 
 interface CEmptyListProps {
   status: string;
@@ -10,7 +10,7 @@ interface CEmptyListProps {
 const CEmptyList = ({ status, description }: CEmptyListProps) => {
   return (
     <div className="flex flex-col justify-center h-full items-center w-full select-none desktop:min-h-[200px]">
-      <Image src={noStreams} alt="icon" />
+      <Image src={emptyLogo} alt="icon" />
       <p className="font-medium text-2xl text-[#8F8F8F]"> {status} </p>
       <p className="mt-2 font-medium text-base text-[#8F8F8F] leading-4 mobile:text-center">
         {description}
