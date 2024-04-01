0c6150a6b4dc1ee36ff034abcb8caab37346c89c
dousdogukan <dousdogukan@gmail.com>
2024-04-01 16:07:11 +0330

    fix: fix checkUserBalance

--- src/containers/CreateStreamMainCard/checkIsUserActive.ts
+++ src/containers/CreateStreamMainCard/checkIsUserActive.ts
@@ -1,6 +1,10 @@
+import BN from 'src/utils/BN';
+
 export type xlmAssetType = {
   asset_type?: string;
   balance?: string;
+  buyingLiabilities?: string;
+  sellingLiabilities?: string;
 };
 
 export const checkIsUserActive = (token: xlmAssetType) => {
@@ -12,7 +16,14 @@ export const checkIsUserActive = (token: xlmAssetType) => {
 };
 
 export const checkUserBalance = (token: xlmAssetType) => {
-  if (Number(token.balance) < 2) {
+  let isSalability;
+  if (token.balance && token.sellingLiabilities && token.buyingLiabilities) {
+    isSalability = new BN(token.balance).minus(
+      new BN(token.sellingLiabilities).plus(token.buyingLiabilities),
+    );
+  }
+
+  if (isSalability?.isLessThan(1.2)) {
     return false;
   }
 
