0e726569f68cfdb4bec96b41193513f3c40b067c
Ruveyda Kışla <ruveydakisla572@gmail.com>
2024-04-24 19:34:38 +0330

    fix: change some packages

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -2,6 +2,8 @@ module.exports = {
   plugins: [
     'postcss-import',
     //'tailwindcss/nesting'(require('postcss-nesting',
+    'tailwindcss/nesting',
+    'postcss-nesting',
     'postcss-nested',
     'autoprefixer',
     'tailwindcss',
