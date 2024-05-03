d0c1fa84e9ab17be6a95c75407e42d85d73372c3
dousdogukan <dousdogukan@gmail.com>
2024-05-03 21:56:44 +0330

    chore: implement getContractIdFromAsset func

---
+++ src/features/getContractIdFromAsset.ts
@@ -0,0 +1,20 @@
+import { Asset, Networks, AssetType } from '@stellar/stellar-sdk';
+
+import getConfigs from './getConfigs';
+
+const getContractIdFromAsset = async (address: string) => {
+  const { horizonServer } = getConfigs();
+
+  const accountData = await horizonServer.loadAccount(address);
+  const accountBalances = accountData.balances;
+
+  const filteredAsset = accountBalances.filter(
+    (asset) => asset.asset_type != 'native' && asset.asset_type != 'liquidity_pool_shares',
+  );
+
+  return filteredAsset.map((asset) =>
+    new Asset(asset.asset_code, asset.asset_issuer).contractId(Networks.TESTNET),
+  );
+};
+
+export default getContractIdFromAsset;
