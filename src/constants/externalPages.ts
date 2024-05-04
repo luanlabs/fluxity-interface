3fb8905f5f1890416c50bd24b89a1d143b2298a7
Ceyda Esen <ceydae317@gmail.com>
2024-05-04 19:53:07 +0330

    chore: change friendbot url

--- src/constants/externalPages.ts
+++ src/constants/externalPages.ts
@@ -4,6 +4,6 @@ export enum ExternalPages {
   COMMUNITY = 'https://discord.gg/JnT7KPEuQs',
   FLUXITY_API = 'https://api.fluxity.finance',
   EXPLORER = 'https://futurenet.stellarchain.io',
-  FRIENDBOT = 'https://friendbot-futurenet.stellar.org/?addr=',
+  FRIENDBOT = 'https://friendbot.stellar.org/?addr=',
   WHITEPAPER = 'https://docs.google.com/document/d/1Yh4wI4RR2WK4CN48HRHJMX78OHcVOYST7MN_U2KSm_4/edit?usp=sharing',
 }
