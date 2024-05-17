3e86e52e4cfb6026cd5ae208253c904beb986874
dousdogukan <dousdogukan@gmail.com>
2024-05-17 12:28:27 +0330

    fix: change type

--- src/features/soroban/getERC20Details.ts
+++ src/features/soroban/getERC20Details.ts
@@ -1,19 +1,14 @@
+import { ITokenDetails } from 'src/models';
+
 import getERC20Symbol from './getERC20Symbol';
 import getERC20Decimal from './getERC20Decimal';
 import getERC20Balance from './getERC20Balance';
-
-export type TokenDetailsType = {
-  address: string;
-  balance: string;
-  symbol: string;
-  decimals: string;
-};
 
 const getERC20Details = async (
   contract: string,
   passPhrase: string,
   userAddress: string,
-): Promise<TokenDetailsType> => {
+): Promise<ITokenDetails> => {
   const balance = getERC20Balance(contract, passPhrase, userAddress);
   const symbol = getERC20Symbol(contract, passPhrase, userAddress);
   const decimals = getERC20Decimal(contract, passPhrase, userAddress);
@@ -25,6 +20,10 @@ const getERC20Details = async (
     balance: result[0],
     symbol: result[1],
     decimals: result[2],
+    name: '',
+    logo: '',
+    _id: '',
+    claimable: false,
   };
 };
 
