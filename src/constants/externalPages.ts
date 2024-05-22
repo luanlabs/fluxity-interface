e7093187ff59c8bb0f4f729c0859dfe089415811
dousdogukan <dousdogukan@gmail.com>
2024-05-22 15:21:46 +0330

    del: delete HorizonLink and getConfig.ts

--- src/constants/externalPages.ts
+++ src/constants/externalPages.ts
@@ -3,8 +3,6 @@ export enum ExternalPages {
   DOCUMENTATION = 'https://docs.fluxity.finance',
   COMMUNITY = 'https://discord.gg/JnT7KPEuQs',
   FLUXITY_API = 'https://api.fluxity.finance',
-  HORIZON_TESTNET = 'https://horizon-testnet.stellar.org/',
-  SOROBAN_TESTNET_RPC = 'https://soroban-testnet.stellar.org/',
   FRIENDBOT = 'https://friendbot.stellar.org/?addr=',
   WHITEPAPER = 'https://docs.google.com/document/d/1Yh4wI4RR2WK4CN48HRHJMX78OHcVOYST7MN_U2KSm_4/edit?usp=sharing',
 }
