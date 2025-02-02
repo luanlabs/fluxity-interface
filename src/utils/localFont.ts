5c42c15979ea3fadbd820905d32acc9fd36b1a97
dousdogukan <dousdogukan@gmail.com>
2025-02-02 12:32:08 +0330

    temp: update font

--- src/utils/localFont.ts
+++ src/utils/localFont.ts
@@ -2,9 +2,8 @@ import localFont from 'next/font/local';
 
 const myFont = localFont({
   src: [
-    { style: 'normal', weight: '400', path: '../../public/font/Aeonik-Regular.ttf' },
-    { style: 'normal', weight: '500', path: '../../public/font/Aeonik-Medium.ttf' },
-    { style: 'normal', weight: '700', path: '../../public/font/Aeonik-Bold.ttf' },
+    { style: 'normal', weight: '400', path: '../../public/font/Aeonik-Regular.woff' },
+    { style: 'normal', weight: '500', path: '../../public/font/Aeonik-Medium.woff' },
   ],
 });
 
